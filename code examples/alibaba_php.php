8b371215c8b79b42811465db7976d3c9ea412296
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-01-10 11:37:29 +0200

    Adding code examples/alibaba_php.php

---
+++ code examples/alibaba_php.php
@@ -0,0 +1,27 @@
+<?php
+
+$params = [
+    'source' => 'universal_ecommerce',
+    'url' => 'https://www.alibaba.com/trade/search?spm=a2700.galleryofferlist.pagemodule_fy23_pc_search_bar.keydown__enter&tab=all&searchtext=phone',
+];
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, "https://realtime.oxylabs.io/v1/queries");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_USERPWD, "user" . ":" . "pass1");
+
+$headers = array();
+$headers[] = "Content-Type: application/json";
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+echo $result;
+
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+?>
