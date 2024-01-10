9f6c0f5e404de241f9bc7d303f5311829243b946
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-01-10 11:37:26 +0200

    Adding code examples/alibaba_nodejs.js

---
+++ code examples/alibaba_nodejs.js
@@ -0,0 +1,18 @@
+import fetch from 'node-fetch';
+
+const username = 'YOUR_USERNAME';
+const password = 'YOUR_PASSWORD';
+const body = {
+  'source': 'universal_ecommerce',
+  'url': 'https://www.alibaba.com/trade/search?spm=a2700.galleryofferlist.pagemodule_fy23_pc_search_bar.keydown__enter&tab=all&searchtext=phone',
+};
+const response = await fetch('https://realtime.oxylabs.io/v1/queries', {
+  method: 'post',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Basic ' + Buffer.from(`${username}:${password}`).toString('base64'),
+  }
+});
+
+console.log(await response.json());
