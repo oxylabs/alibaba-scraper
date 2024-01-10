2ea57db8945c5e09609f4f5758b6461f7c5fc60b
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2024-01-10 11:37:34 +0200

    Adding code examples/alibaba_curl.sh

---
+++ code examples/alibaba_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal_ecommerce", "url": "https://www.alibaba.com/trade/search?spm=a2700.galleryofferlist.pagemodule_fy23_pc_search_bar.keydown__enter&tab=all&searchtext=phone"}'
